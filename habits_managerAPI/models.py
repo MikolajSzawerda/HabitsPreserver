9e03d107e00744f7a8cdb2e72d5a9bca5753da7a
Mikołaj Szawerda <01169246@pw.edu.pl>
2022-02-24 18:21:54 +0100

    Created habit defintion

--- habits_managerAPI/models.py
+++ habits_managerAPI/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class ActivityItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+
+class Habit(ActivityItem):
+   pass
+
+
+class FulfillmentLevel(ActivityItem):
+    value = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(3)])
+
+
+class HabitFulfillment(ActivityItem):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    fulfillment_level = models.ForeignKey(FulfillmentLevel, on_delete=models.CASCADE)
