0dd53d003d78c175ffc64e1b3be2ad5707a28406
Mikołaj Szawerda <01169246@pw.edu.pl>
2022-02-24 18:41:43 +0100

    admin init

--- habits_managerAPI/admin.py
+++ habits_managerAPI/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Habit, HabitFulfillment, FulfillmentLevel
 
 # Register your models here.
+admin.site.register(Habit)
+admin.site.register(FulfillmentLevel)
