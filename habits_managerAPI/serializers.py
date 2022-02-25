3e5dde2522f938bb715a5917664e17a765e15c3e
Mikołaj Szawerda <01169246@pw.edu.pl>
2022-02-25 22:20:05 +0100

    Serializer refactor

--- habits_managerAPI/serializers.py
+++ habits_managerAPI/serializers.py
@@ -1,4 +1,3 @@
-from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from .models import FulfillmentLevel, Habit, HabitFulfillment
 
@@ -30,16 +29,11 @@ class HabitSerializer(ModelSerializer):
         return habit
 
 
-class SUDOHabitSerializer(ModelSerializer):
-    fulfillemnts = FulfillmentSerializer(many=True)
+class SUDOHabitSerializer(HabitSerializer):
     class Meta:
         model = Habit
         fields = ['id', 'name', 'user', 'description', 'fulfillemnts']
 
     def create(self, validated_data):
-        user=self.context['request'].user
-        fulfillemnts_data = validated_data.pop('fulfillemnts')
-        habit = Habit.objects.create(user=user, **validated_data)
-        for fulfillemnt in fulfillemnts_data:
-            HabitFulfillment.objects.create(habit=habit, **fulfillemnt)
-        return habit
+        pass
+
