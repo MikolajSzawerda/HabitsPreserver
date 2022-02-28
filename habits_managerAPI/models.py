2a5a9c84650be0b8413310bda23ab24fb8a75de3
Mikołaj Szawerda <01169246@pw.edu.pl>
2022-02-28 21:19:00 +0100

    Renamed models, improved formatting

--- habits_managerAPI/models.py
+++ habits_managerAPI/models.py
@@ -2,30 +2,33 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
 
+
 class ActivityItem(models.Model):
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=1000)
-
-    class Meta:
-        abstract = True
+    description = models.CharField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        abstract = True
 
 
 class Habit(ActivityItem):
-   user = models.ForeignKey(User, on_delete= models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             blank=True, null=True)
 
 
 class FulfillmentLevel(ActivityItem):
-    value = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(3)])
+    value = models.IntegerField(validators=[MinValueValidator(0),
+                                            MaxValueValidator(3)])
 
 
 class HabitFulfillment(ActivityItem):
-    habit = models.ForeignKey(Habit, on_delete=models.CASCADE, related_name='fulfillemnts')
-    fulfillment_level = models.ForeignKey(FulfillmentLevel, on_delete=models.CASCADE)
-    description = models.CharField(max_length=1000, null=True, blank=True)
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE,
+                              related_name='fulfillments')
+    fulfillment_level = models.ForeignKey(FulfillmentLevel,
+                                          on_delete=models.CASCADE)
 
 
 class HabitAction(models.Model):
