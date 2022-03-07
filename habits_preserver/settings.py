904d9efd748a33021732a5ebbd65c7f26de9c9fe
Mikołaj Szawerda <01169246@pw.edu.pl>
2022-03-07 23:53:05 +0100

    React init v1

--- habits_preserver/settings.py
+++ habits_preserver/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'habits_presentation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'habits_managerAPI',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- habits_preserver/urls.py
+++ habits_preserver/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('habits_managerAPI.urls'))
+    path('api/', include('habits_managerAPI.urls')),
+    path('', include('habits_presentation.urls')),
 ]
